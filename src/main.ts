e95daf0a8385df591c77cb6f2ef5fbef3daec939
Philip Bagus Kristianto <bagus09051998@gmail.com>
2022-09-28 16:59:12 +0700

    menambahkan env ke main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+const env = dotenv.parse(fs.readFileSync('.env'));
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  await app.listen(env.PORT);
 }
 bootstrap();
